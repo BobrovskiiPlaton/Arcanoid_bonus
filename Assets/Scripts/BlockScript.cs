80e5e516b5123a749b89a06e41719b665a2daf94
KreKer_ <platon.bob@gmail.com>
2025-12-08 16:34:22 +0300

    oaoaoaoao

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI; // using TMPro;
+public class BlockScript : MonoBehaviour
+{
+    public GameObject textObject;
+    TMP_Text textComponent; // TMP_Text textComponent;
+    public int hitsToDestroy;
+    public int points;
+
+    private PlayerScript playerScript;
+    void Start()
+    {
+        if (textObject != null)
+        {
+            textComponent = textObject.GetComponent<TMP_Text>();
+            textComponent.text = hitsToDestroy.ToString();
+        }
+
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            hitsToDestroy--;
+            if (hitsToDestroy == 0)
+            {
+                print(points);
+                Destroy(gameObject);
+                playerScript.BlockDestroyed(points);
+            }
+            else if (textComponent != null)
+                textComponent.text = hitsToDestroy.ToString();
+        }
+    }
+}
